d5d7baa85c5b338fc2915121fcc208b9992745cc
voltage-poppy <whwfig@gmail.com>
2025-04-10 10:56:07 +0800

    Update mnist_loader.py

--- src/mnist_loader.py
+++ src/mnist_loader.py
@@ -16,22 +16,24 @@ import gzip
 # Third-party libraries
 import numpy as np
 
-def load_data():
+def load_data() -> tuple:
     """Return the MNIST data as a tuple containing the training data,
     the validation data, and the test data.
 
-
-print(load_data_wrapper()[0])
     The ``training_data`` is returned as a tuple with two entries.
-    The first entry contains the actual training images.  This is a
+    The 1st entry contains the actual training images.  This is a
     numpy ndarray with 50,000 entries.  Each entry is, in turn, a
     numpy ndarray with 784 values, representing the 28 * 28 = 784
     pixels in a single MNIST image.
 
-    The second entry in the ``training_data`` tuple is a numpy ndarray
+    training_data[0].shape is (50000, 784)
+
+    The 2nd entry in the ``training_data`` tuple is a numpy ndarray
     containing 50,000 entries.  Those entries are just the digit
     values (0...9) for the corresponding images contained in the first
     entry of the tuple.
+
+    training_data[1].shape is (50000,)
 
     The ``validation_data`` and ``test_data`` are similar, except
     each contains only 10,000 images.
@@ -48,7 +50,8 @@ print(load_data_wrapper()[0])
     f.close()
     return (training_data, validation_data, test_data)
 
-def load_data_wrapper():
+
+def load_data_wrapper() -> tuple:
     """Return a tuple containing ``(training_data, validation_data,
     test_data)``. Based on ``load_data``, but the format is more
     convenient for use in our implementation of neural networks.
@@ -70,20 +73,31 @@ def load_data_wrapper():
     turn out to be the most convenient for use in our neural network
     code."""
     tr_d, va_d, te_d = load_data()
+
     training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
     training_results = [vectorized_result(y) for y in tr_d[1]]
     training_data = list(zip(training_inputs, training_results))
+
     validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
     validation_data = list(zip(validation_inputs, va_d[1]))
+
     test_inputs = [np.reshape(x, (784, 1)) for x in te_d[0]]
     test_data = list(zip(test_inputs, te_d[1]))
+
     return (training_data, validation_data, test_data)
 
-def vectorized_result(j):
+def vectorized_result(j: int) -> np.ndarray:
     """Return a 10-dimensional unit vector with a 1.0 in the jth
     position and zeroes elsewhere.  This is used to convert a digit
     (0...9) into a corresponding desired output from the neural
-    network."""
+    network.
+    e.g.
+    0 -> [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    1 -> [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+    2 -> [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+    ...
+    9 -> [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    """
     e = np.zeros((10, 1))
     e[j] = 1.0
     return e
